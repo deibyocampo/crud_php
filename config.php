f69886366992ebc8d88bef3f57c0f75ee47b9100
deibyocampo <deiby13_96@hotmail.com>
2021-10-23 23:54:25 -0500

    create files the configurations

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => 'root',
+        'name' => 'tutorial_crud',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
+];
+
+?>

77b996e34ac640065bcd767693ecdb721f2d5a22
deibyocampo <deiby13_96@hotmail.com>
2021-10-24 23:03:55 -0500

    create form a insert data students

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 <!-- aqui va el codigo de la aplicacion -->
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <a href="create.php"  class="btn btn-primary mt-4">Crear alumno</a>
+      <hr>
+    </div>
+  </div>
+</div>
+
 <?php include "templates/footer.php"; ?>

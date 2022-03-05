107ae304203e096e629c2b061089d68e6b21fde3
deibyocampo <deiby13_96@hotmail.com>
2022-03-04 23:41:19 -0500

    add html code on the home page to display the list of registered students

--- index.php
+++ index.php
@@ -42,11 +42,49 @@ if ($error) {
 }
 ?>
 
+<!-- codigo del boton para crear un boton -->
 <div class="container">
   <div class="row">
     <div class="col-md-12">
       <a href="create.php"  class="btn btn-primary mt-4">Crear alumno</a>
       <hr>
+    </div>
+  </div>
+</div>
+
+<!-- Lista de los alumnos registrados -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="mt-3">Lista de alumnos</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Edad</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($alumnos && $sentencia->rowCount() > 0) {
+            foreach ($alumnos as $fila) {
+              ?>
+              <tr>
+                <td><?php echo escapar($fila["id"]); ?></td>
+                <td><?php echo escapar($fila["nombre"]); ?></td>
+                <td><?php echo escapar($fila["apellido"]); ?></td>
+                <td><?php echo escapar($fila["email"]); ?></td>
+                <td><?php echo escapar($fila["edad"]); ?></td>
+              </tr>
+              <?php
+            }
+          }
+          ?>
+        <tbody>
+      </table>
     </div>
   </div>
 </div>

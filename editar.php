f7c5402b81ac2680d261057e4e2b9015c7c974bc
deibyocampo <deiby13_96@hotmail.com>
2022-03-06 19:51:05 -0500

    update php code conection the database

--- editar.php
+++ editar.php
@@ -1,94 +1,88 @@
-<?php require "templates/header.php"; ?>
-<!-- código de la página -->
-
-<?php 
+<?php
 include 'funciones.php';
 
 $config = include 'config.php';
 
 $resultado = [
-    'error' => false,
-    'mensaje' => ''
+  'error' => false,
+  'mensaje' => ''
 ];
 
 if (!isset($_GET['id'])) {
-    $resultado['error'] = true;
-    $resultado['mensaje'] = 'El alumno no existe';
+  $resultado['error'] = true;
+  $resultado['mensaje'] = 'El alumno no existe';
 }
 
- // Conectamos ala base de datos y actualizamos el alumno con los nuevos datos
-if(isset($_POST['submit'])) {
-    try {
-        $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname' . $config['db']['name'];
-        $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-
-    /* usaremos la sentencia SQL UPDATE para actualizar los valores del alumno cuyo id
-    corresponde con lo que estamos editando */
-        $alumno = [
-            "id"        => $_GET['id'],
-            "nombre"    => $_POST['nombre'],
-            "apellido"  => $_POST['apellido'],
-            "email"     => $_POST['email'],
-            "edad"      => $_POST['edad']
-          ];
-          
-        $consultaSQL = "UPDATE alumnos SET
-              nombre = :nombre,
-              apellido = :apellido,
-              email = :email,
-              edad = :edad,
-              updated_at = NOW()
-              WHERE id = :id";
-          
-        $consulta = $conexion->prepare($consultaSQL);
-        $consulta->execute($alumno);
-
-    } catch(PDOException $error) {
-        $resultado['error'] = true;
-        $resultado['mensaje'] = $error->getMessage();
-    }
-}
-
-try{
-    $dns = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+if (isset($_POST['submit'])) {
+  try {
+    $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
     $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
 
-    $id = $_GET['id'];
-    $consultaSQL = "SELECT * FROM alumnos WHERE id =" . $id;
+    $alumno = [
+      "id"        => $_GET['id'],
+      "nombre"    => $_POST['nombre'],
+      "apellido"  => $_POST['apellido'],
+      "email"     => $_POST['email'],
+      "edad"      => $_POST['edad']
+    ];
+    
+    $consultaSQL = "UPDATE alumnos SET
+        nombre = :nombre,
+        apellido = :apellido,
+        email = :email,
+        edad = :edad,
+        updated_at = NOW()
+        WHERE id = :id";
+    
+    $consulta = $conexion->prepare($consultaSQL);
+    $consulta->execute($alumno);
 
-    $sentencia = $conexion->prepare($consultaSQL);
-    $sentencia->execute();
-
-    $alumno = $sentencia->fetch(PDO::FETCH_ASSOC);
-
-    if (!$alumno){
-        $resultado['error'] = true;
-        $resultado['mensaje'] = 'No se ha encontrado el alumno';
-    }
-   
-} catch(PDOException $error) {
+  } catch(PDOException $error) {
     $resultado['error'] = true;
     $resultado['mensaje'] = $error->getMessage();
+  }
 }
 
+try {
+  $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+  $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+    
+  $id = $_GET['id'];
+  $consultaSQL = "SELECT * FROM alumnos WHERE id =" . $id;
+
+  $sentencia = $conexion->prepare($consultaSQL);
+  $sentencia->execute();
+
+  $alumno = $sentencia->fetch(PDO::FETCH_ASSOC);
+
+  if (!$alumno) {
+    $resultado['error'] = true;
+    $resultado['mensaje'] = 'No se ha encontrado el alumno';
+  }
+
+} catch(PDOException $error) {
+  $resultado['error'] = true;
+  $resultado['mensaje'] = $error->getMessage();
+}
 ?>
+
+<?php require "templates/header.php"; ?>
 
 <?php
-    if ($resultado['error']) {
-    ?>
-    <div class="container mt-2">
-        <div class="row">
-        <div class="col-md-12">
-            <div class="alert alert-danger" role="alert">
-            <?= $resultado['mensaje'] ?>
-            </div>
+if ($resultado['error']) {
+  ?>
+  <div class="container mt-2">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="alert alert-danger" role="alert">
+          <?= $resultado['mensaje'] ?>
         </div>
-        </div>
+      </div>
     </div>
-    <?php
-    }
+  </div>
+  <?php
+}
 ?>
-
 
 <?php
 if (isset($_POST['submit']) && !$resultado['error']) {
@@ -105,7 +99,6 @@ if (isset($_POST['submit']) && !$resultado['error']) {
   <?php
 }
 ?>
-
 
 <?php
 if (isset($alumno) && $alumno) {

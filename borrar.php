bc5e37c4c18e710df4f405a6dc8a72e84392c8d0
deibyocampo <deiby13_96@hotmail.com>
2022-03-07 17:43:08 -0500

    conect data base and delete student

--- borrar.php
+++ borrar.php
@@ -1,3 +1,31 @@
+<?php
+include 'funciones.php';
+
+$config = include 'config.php';
+
+$resultado = [
+    'error' => false,
+    'mensaje' => ''
+];
+
+try{
+    $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
+  $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+    
+  $id = $_GET['id'];
+  $consultaSQL = "DELETE FROM alumnos WHERE id =" . $id;
+
+  $sentencia = $conexion->prepare($consultaSQL);
+  $sentencia->execute();
+
+  header('Location: /index.php');
+  
+}catch(PDOException $error){
+    $resultado['error'] = true;
+    $resultado['mensaje'] = $error->getMessage();
+}
+?>
+
 <?php require "templates/header.php"; ?>
 <!-- código de la página -->
 <?php require "templates/footer.php"; ?>
